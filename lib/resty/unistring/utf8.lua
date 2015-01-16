f3e1cf5e3404027dac5bb3efab8fee3544f5de28
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2015-01-16 02:43:36 +0200

    Implemented slug function (works only with OpenResty).

--- lib/resty/unistring/utf8.lua
+++ lib/resty/unistring/utf8.lua
@@ -3,10 +3,21 @@
 local sub  = string.sub
 local str  = require "resty.unistring.str"
 local case = require "resty.unistring.case"
+local norm = require "resty.unistring.norm"
+local nrmz = norm.u8_normalize
 local find = string.find
-if not utf8 then
-    utf8 = {}
+local utf8 = utf8 or {}
+if not utf8.charpattern then
     utf8.charpattern = "[\0-\x7F\xC2-\xF4][\x80-\xBF]*"
+end
+--if not utf8.char then
+--    function utf8.char(...)
+--        for i = 1, select("#", ...) do
+--            select(i, ...)
+--        end
+--    end
+--end
+if not utf8.len then
     function utf8.len(s, i, j)
         local x = sub(s, i or 1, j or -1)
         local e = str.u8_check(x, #x)
@@ -26,3 +37,22 @@ if not utf8.upper then
         return case.u8_toupper(s)
     end
 end
+if not utf8.slug then
+    function utf8.slug(s)
+        if not ngx then return nil end
+        local gsub = ngx.re.gsub
+        s = nrmz("nfkd", s)
+        if not s then return nil end
+        s = gsub(s, "\\p{Mn}", "", "u")
+        if not s then return nil end
+        s = gsub(s, "\\W", "-")
+        if not s then return nil end
+        s = gsub(s, "\\-{2,}", "-")
+        if not s then return nil end
+        local i, j = 1, #s
+        if sub(s, i, i) == "-" then i = i + 1 end
+        if sub(s, j, j) == "-" then j = j - 1 end
+        return sub(s, i, j)
+    end
+end
+if not _G.utf8 then _G.utf8 = utf8 end
